d3d2245ff239b9ad687d6e44355db27c047b640b
Elias Palavecino <eliaspalavecino125@gmail.com>
2024-04-22 21:53:03 -0300

    asd

--- src/components/contacto.js
+++ src/components/contacto.js
@@ -1,6 +1,12 @@
 import './contacto.css'
-import  telefono from "./img/mail-contact.png";
-import  whatsapp from "./img/mail-contact.png";
+import  cellphone from "./img/smartphone.png";
+import  whatsapp from "./img/whatsapp.png";
+import  gmail from "./img/gmail.png";
+import  linkedin from "./img/linkedin.png";
+import  github from "./img/github.png";
+import  upwork from "./img/upwork.png";
+import  fiverr from "./img/fiverr.png";
+
 
 
 function Contacto() {
@@ -8,8 +14,27 @@ function Contacto() {
       <div className='contacto'>
         <h1 className='titulo'>contacto</h1>
         <div className='telefono-barra'>
-          <img src={telefono}/> <p className='number'>3876404816</p> <img src={whatsapp}/>
+          <img src={cellphone} className='social-logo'/> <p className='number'>+54 3876-404816 </p> <img src={whatsapp} className='social-logo'/>
         </div>
+        <div className='mail-barra'>
+          <img src={gmail} className='social-logo'/> <p className='number'>eliaspalavecino125@gmail.com</p> 
+        </div>
+        <div className='socialmedia-barra'>
+          <div className='social-block'>
+            <img src={linkedin} className='social-logo'/> <a href="https://linkedin.com/in/elias-samuel-palavecino-34978422b" className='link-text'>LinkedIn</a> 
+          </div>
+          <div className='social-block'>
+            <img src={github} className='social-logo'/> <a href="https://github.com/eliassam125" className='link-text'>Github</a>
+          </div>
+          <div className='social-block'>
+            <img src={upwork} className='social-logo'/> <a href="https://www.upwork.com/freelancers/~01d267d9c66ec8e14e?mp_source=share" className='link-text'>UpWork</a> 
+          </div>
+          <div className='social-block'>
+            <img src={fiverr} className='social-logo'/> <a href="https://www.fiverr.com/elias_sam125" className='link-text'>Fiverr</a> 
+          </div>
+          
+        </div>
+        
         
 
 
